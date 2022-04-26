e41905efd710c39fbeec448249ec5063e807db06
Nicolas Larrosa <nicolarro20@gmail.com>
2022-04-26 19:00:02 -0300

    correciones

--- src/componentes/CheckOut/Checkout.js
+++ src/componentes/CheckOut/Checkout.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
-import {CarritoContexto}  from "../../contexto/CarritoContexto";
+import { CarritoContexto } from "../../contexto/CarritoContexto";
 import { Navigate } from "react-router-dom";
 /* import { generarOrden } from "../../firebase/generarOrden"; */
-import { MensajeCompra }  from "../CheckOut/MensajeCompra";
+import { MensajeCompra } from "../CheckOut/MensajeCompra";
 /* import { validar } from "./Validacion"; */
-import { collection, addDoc, doc, getDoc,updateDoc, Timestamp} from "firebase/firestore";
-import {db} from "../../firebase/config.js";
-                  
+import {
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  updateDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase/config.js";
+
 const Checkout = () => {
   const { carrito, totalCarrito, vaciarCarrito } = useContext(CarritoContexto);
 
-  const [orderId, setOrderId] = useState(null); 
+  const [orderId, setOrderId] = useState(null);
 
   const navigate = useNavigate();
 
@@ -37,11 +44,11 @@ const Checkout = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-/*     validar(values) && */
-/*       generarOrden(values, carrito, totalCarrito, setOrderId, vaciarCarrito); */
+    /*     validar(values) && */
+    /*       generarOrden(values, carrito, totalCarrito, setOrderId, vaciarCarrito); */
 
-/*       console.log(generarOrden) */
-    
+    /*       console.log(generarOrden) */
+
     const orden = {
       items: carrito,
       total: totalCarrito(),
@@ -54,80 +61,76 @@ const Checkout = () => {
 
     const orderDb = collection(db, "ordenes");
 
-    console.log(orderDb)
+    console.log(orderDb);
 
-
-    
-     carrito.forEach((item) => {
-         const docRef = doc(db, 'productos', item.id)
-         getDoc(docRef)
-             .then((dbDoc) => {
-            if (doc.data().stock >= item.cantidad) {    
-                 updateDoc(docRef, {stock: dbDoc.data().stock - item.cantidad})
-                 console.log(item.stock)
-             }
-            else {
-              alert("No hay stock de este item")
+    carrito.forEach(
+      (item) => {
+        const docRef = doc(db, "productos", item.id);
+        getDoc(docRef).then((dbDoc) => {
+          if (doc.data().stock >= item.cantidad) {
+            updateDoc(docRef, { stock: dbDoc.data().stock - item.cantidad });
+            console.log(item.stock);
+          } else {
+            alert("No hay stock de este item");
           }
-     })
-    },
+        });
+      },
 
-    addDoc(orderDb, orden)
-    .then((doc) => {
-      setOrderId(doc.id);
-      vaciarCarrito()
-  })
-  )
-  };
-
-    if (orderId) {
-      return <MensajeCompra order={orderId} />;
-    }
-
-    if (carrito.length === 0) {
-      return <Navigate to="/" />;
-    }
-
-    return (
-      <div>
-        <br />
-        <h3> CHECKOUT </h3>
-        <br />
-        <form onSubmit={handleSubmit}>
-          <input
-            className="form-control my-3"
-            type={"text"}
-            placeholder="Ingresar Nombre"
-            value={values.nombre}
-            onChange={handleValueChange}
-            autoComplete="off"
-          />
-          <input
-            className="form-control my-3"
-            type={"email"}
-            value={values.email}
-            placeholder="Ingresar email"
-            onChange={handleValueChange}
-            autoComplete="off"
-          />
-          <input
-            className="form-control my-3"
-            type={"tel"}
-            value={values.tel}
-            placeholder="Ingresar Telefono"
-            onChange={handleValueChange}
-            autoComplete="off"
-          />
-        </form>
-
-        <button className="btn btn-primary" type="submit">
-          ENVIAR
-        </button>
-        <button className="btn btn-primary" onClick={handleNavigate}>
-          VOLVER
-        </button>
-      </div>
+      addDoc(orderDb, orden).then((doc) => {
+        setOrderId(doc.id);
+        vaciarCarrito();
+      })
     );
   };
+
+  if (orderId) {
+    return <MensajeCompra order={orderId} />;
+  }
+
+  if (carrito.length === 0) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div>
+      <br />
+      <h3> CHECKOUT </h3>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control my-3"
+          type={"text"}
+          placeholder="Ingresar Nombre"
+          value={values.nombre}
+          onChange={handleValueChange}
+          autoComplete="off"
+        />
+        <input
+          className="form-control my-3"
+          type={"email"}
+          value={values.email}
+          placeholder="Ingresar email"
+          onChange={handleValueChange}
+          autoComplete="off"
+        />
+        <input
+          className="form-control my-3"
+          type={"tel"}
+          value={values.tel}
+          placeholder="Ingresar Telefono"
+          onChange={handleValueChange}
+          autoComplete="off"
+        />
+      </form>
+
+      <button className="btn btn-primary" type="submit">
+        ENVIAR
+      </button>
+      <button className="btn btn-primary" onClick={handleNavigate}>
+        VOLVER
+      </button>
+    </div>
+  );
+};
 
 export default Checkout;
